823454331aeb5502cebea027e86084f56a57e22c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-10 19:52:27 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -36,7 +36,7 @@ async def mention_user(user_id):
     except Exception as e:
         print(f"Failed to mention user: {e}")
 
-@xteam_cmd(pattern="ping(|x|s)$", chats=[], type=["official", "assistant"])
+@xteam_cmd(pattern="p(|x|s)$", chats=[], type=["official", "assistant"])
 async def _(event):
     ultroid_bot.parse_mode = CustomMarkdown()
     user_id = OWNER_ID
@@ -64,3 +64,50 @@ async def _(event):
     uptime = time_formatter((time.time() - start_time) * 1000)
     await asyncio.sleep(1)
     await x.edit(get_string("kping").format(end))
+
+import asyncio
+from .  import ultroid_cmd, eor, time_formatter, start_time, OWNER_NAME
+import time
+from secrets import choice
+from telethon.tl.types import InputMessagesFilterVideo, InputMessagesFilterVoice
+from telethon.tl.types import InputMessagesFilterPhotos
+"""
+papnya = [
+        pap
+        async for pap in e.client.iter_messages(
+            "@CeweLogoPack", filter=InputMessagesFilterPhotos
+        )
+    ]
+
+ppcpnya = [
+        ppcp
+        async for ppcp in event.client.iter_messages(
+            "@ppcpcilik", filter=InputMessagesFilterPhotos
+        )
+    ]
+
+desahcewe = [
+        desah
+        async for desah in event.client.iter_messages(
+            "@desahancewesangesange", filter=InputMessagesFilterVoice
+        )
+    ]
+"""
+@ultroid_cmd(pattern="ping")
+async def wping(e):
+    asupannya = [
+        asupan
+        async for asupan in e.client.iter_messages(
+            "@xcryasupan", filter=InputMessagesFilterVideo
+        )
+    ]
+    start = time.time()
+    x = await e.reply("Pong!")
+    end = round((time.time() - start) * 1000)
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    #await asyncio.sleep(1)
+    try:
+        await x.edit(get_string("ping").format(end, uptime, f"{OWNER_NAME}"), file=choice(asupannya))
+        #await x.edit(f"**Ping :** `{end}ms`\n**Uptime :** `{uptime}`\n**Owner** :`{OWNER_NAME}`", file=choice(asupannya))
+    except Exception as e:
+        await x.edit(f"**Ping Error:** {e}")
