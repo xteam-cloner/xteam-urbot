966cae6a7206d087de45e132220bfe1c71b5b76c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-13 16:01:20 +0700

    Create image.py

---
+++ plugins/image.py
@@ -0,0 +1,28 @@
+import telethon
+from PIL import Image
+import torch  # Asumsikan menggunakan PyTorch untuk model
+from torchvision import transforms
+
+# ... (inisialisasi klien Telethon dan model FluxAI/Vision Transformer)
+
+@client.on(events.NewMessage(pattern='^.image'))
+async def handler(event):
+    # Dapatkan gambar
+    file = await event.client.download_media(event.message.media)
+    image = Image.open(file)
+
+    # Preproses gambar
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        # ... transformasi lainnya
+    ])
+    image_tensor = transform(image)
+
+    # Lakukan inferensi dengan model
+    with torch.no_grad():
+        output = model(image_tensor.unsqueeze(0))
+
+    # Pascaproses output dan kirimkan respons
+    caption = decode_output(output)  # Fungsi untuk mendecode output model
+    await event.reply(caption)
