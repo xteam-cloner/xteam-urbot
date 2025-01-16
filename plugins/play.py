3f3b2a89b8c3fe878d12d699e2030063a01a93ae
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-16 15:05:18 +0700

    Create play.py

---
+++ plugins/play.py
@@ -0,0 +1,30 @@
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+
+from pytgcalls import filters as fl
+from pytgcalls import idle
+from pytgcalls import PyTgCalls
+from pytgcalls.types import ChatUpdate
+from pytgcalls.types import GroupCallParticipant
+from pytgcalls.types import MediaStream
+from pytgcalls.types import Update
+
+app = Client(
+    'py-tgcalls',
+    api_id=123456789,
+    api_hash='abcdef12345',
+)
+call_py = PyTgCalls(app)
+test_stream = 'http://docs.evostream.com/sample_content/assets/' \
+              'sintel1m720p.mp4'
+
+
+@app.on_message(filters.regex('!play'))
+async def play_handler(_: Client, message: Message):
+    await call_py.play(
+        message.chat.id,
+        MediaStream(
+            test_stream,
+        ),
+    )
