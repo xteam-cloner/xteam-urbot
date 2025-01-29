a7cb671e6c56a8a5f9c4653262acb1ed711ade0d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-29 19:04:30 +0700

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -95,7 +95,7 @@ apis = [
 @in_pattern("ping", owner=False)
 async def ping(e):
     TLINK = inline_pic()
-    MSG = "**inline**"
+    MSG = "**━━━━✿ ᴜꜱᴇʀʙᴏᴛ ✿━━━**"
     WEB0 = wb(
         "https://telegra.ph/file/8d7b534e34e13316a7dd2.jpg", 0, "image/jpg", []
     )
