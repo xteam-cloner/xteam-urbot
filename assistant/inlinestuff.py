66edcfa3be5c947372aa808c0826be4e040b8704
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-01-29 15:59:42 +0700

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -96,7 +96,7 @@ async def ping(e):
         await e.builder.article(
             title="Inline",
             description="PING",
-            file=xteam,
+            file="https://telegra.ph/file/8d7b534e34e13316a7dd2.jpg",
             text=OWNER_NAME,
             buttons=PING_ALIVE,
         )
