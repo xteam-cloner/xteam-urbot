021b9bb5c619bbdcd89750e5cb1d8fdd96fcdf69
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-10 23:40:02 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -88,7 +88,7 @@ async def wping(e):
     uptime = time_formatter((time.time() - start_time) * 1000)
     #await asyncio.sleep(1)
     try:
-        await x.edit(get_string("ping").format(f"{BOT_NAME}", end, uptime, f"{inline_mention(ultroid_bot.me)}"), file=choice(asupannya))
+        await x.edit(get_string("ping").format(f"{{inline_mention(ultroid_bot.asst)}}", end, uptime, f"{inline_mention(ultroid_bot.me)}"), file=choice(asupannya))
         #await x.edit(f"**Ping :** `{end}ms`\n**Uptime :** `{uptime}`\n**Owner** :`{OWNER_NAME}`", file=choice(asupannya))
     except Exception as e:
         await x.edit(f"**Ping Error:** {e}")
