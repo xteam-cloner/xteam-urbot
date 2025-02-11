9502ce55858d0daba49dcce9de788a9a15b46701
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-11 14:02:23 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -109,7 +109,7 @@ async def wping(e):
     try:
        await client.send_message(
     e.chat.id,
-    f"<blockquote> Ping : {end}ms\nUptime : {uptime}\nOwner :{OWNER_NAME}</blockquote>",
+    f"<blockquote> Ping : {end}ms\nUptime : {uptime}\nOwner :{inline_mention(ultroid_bot.me)}</blockquote>",
     parse_mode="html",
     file=choice(asupannya)
 )
