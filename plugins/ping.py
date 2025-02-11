60ff3d21924c9734f66880be0584543d4f15027c
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-11 14:37:38 +0700

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -109,7 +109,7 @@ async def wping(e):
     uptime = time_formatter((time.time() - start_time) * 1000)
     #await asyncio.sleep(1)
     try:
-       await x.edit(
+       await client.send_message(
     e.chat.id,
     f"<blockquote> Ping : {end}ms\nUptime : {uptime}\nOwner :{OWNER_NAME}</blockquote>",
     parse_mode="html",
