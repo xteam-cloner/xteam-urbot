21279fe183898ff28cf22b63073274646a650bf7
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-20 15:52:22 +0700

    Update loader.py

--- pyUltroid/startup/loader.py
+++ pyUltroid/startup/loader.py
@@ -116,25 +116,3 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
         if pmbot:
             Loader(path="assistant/pmbot.py").load(log=False)
 
-    # vc bot
-    if vcbot and (vcClient and not vcClient.me.bot):
-        try:
-            import pytgcalls  # ignore: pylint
-
-            if os.path.exists("vcbot"):
-                if os.path.exists("vcbot/.git"):
-                    subprocess.run("cd vcbot && git pull", shell=True)
-                else:
-                    rmtree("vcbot")
-            if not os.path.exists("vcbot"):
-                subprocess.run(
-                    "git clone https://github.com/xteam-cloner/VcBotku vcbot", shell=True
-                )
-            try:
-                if not os.path.exists("vcbot/downloads"):
-                    os.mkdir("vcbot/downloads")
-                Loader(path="vcbot", key="VCBot").load(after_load=_after_load)
-            except FileNotFoundError as e:
-                LOGS.error(f"{e} Skipping VCBot Installation.")
-        except ModuleNotFoundError:
-            LOGS.error("'pytgcalls' not installed!\nSkipping loading of VCBOT.")
