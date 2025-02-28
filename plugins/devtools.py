13ba90bc56fb0a11a9b76b1b48cac82a749590ed
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-28 18:43:38 +0700

    Update devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -293,7 +293,7 @@ async def _(event):
     tmt = tima * 1000
     timef = time_formatter(tmt)
     timeform = timef if not timef == "0s" else f"{tmt:.3f}µs"
-    final_output = "<blockquote>Python\n\n{}</blockquote>\n\n<blockquote>Output\n{}</blockquote>\n\n<blockquote>Timsec:\n{}\n</blockquote>".format(
+    final_output = "<blockquote>Input: {}</blockquote>\n\n<blockquote>Output: {}</blockquote>\n<blockquote>Timsec:{}\n</blockquote>".format(
         cmd,
         evaluation,
         timeform,
