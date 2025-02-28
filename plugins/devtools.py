0862f59fb6b8b36bbdc003195fd4c38bf457da0d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-02-28 16:54:34 +0700

    Update devtools.py

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -293,7 +293,12 @@ async def _(event):
     tmt = tima * 1000
     timef = time_formatter(tmt)
     timeform = timef if not timef == "0s" else f"{tmt:.3f}µs"
-    final_output = (f"<blockquote>Python\n{cmd}\n\nOutput: {evaluation}\n\nTimesec\n{timeform}</blockquote>", parse_mode="html")
+    final_output = "<blockquote>__►__ **EVAL** (__in {}__)\n```{}``` \n\n __►__ **OUTPUT**: \n```{}``` \n</blockquote>".format(
+        cmd,
+        evaluation,
+        timeform,
+        parse_mode="html",
+    )
     if len(final_output) > 4096:
         final_output = evaluation
         with BytesIO(str.encode(final_output)) as out_file:
