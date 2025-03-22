8eff78001e787891b370503cf154cbcee6cc669e
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-22 21:04:24 +0700

    Update helper.py

--- plugins/helper.py
+++ plugins/helper.py
@@ -4,7 +4,34 @@ from datetime import datetime
 from html import unescape
 from random import choice
 from re import compile as re_compile
+import re
+import time
+from datetime import datetime
+from os import remove
 
+from git import Repo
+from telethon import Button
+from telethon.tl.types import InputWebDocument, Message
+from telethon.utils import resolve_bot_file_id
+
+from pyUltroid._misc._assistant import callback, in_pattern
+from pyUltroid.dB._core import HELP, LIST
+from pyUltroid.fns.helper import gen_chlog, time_formatter, updater
+from pyUltroid.fns.misc import split_list
+
+from . import (
+    HNDLR,
+    LOGS,
+    OWNER_NAME,
+    InlinePlugin,
+    asst,
+    get_string,
+    inline_pic,
+    split_list,
+    start_time,
+    udB,
+)
+from ._help import _main_help_menu
 from telethon import Button
 from telethon.errors.rpcerrorlist import (
     BotMethodInvalidError,
@@ -14,21 +41,18 @@ from telethon.errors.rpcerrorlist import (
 from pyUltroid.dB._core import HELP
 from . import OWNER_NAME, ultroid_cmd, get_string
 
+
 _main_help_menu = [[Button.inline("ᴍᴏᴅᴜʟᴇꜱ", data="uh_Official_")]]
 
 
 @ultroid_cmd(pattern="helper$")
-async def _help(ult):
-    try:
-        await ult.reply(
-            get_string("inline_4").format(
-                OWNER_NAME,
-                len(HELP.get("Official", [])),
-                len(HELP.get("Addons", [])),
-                80,  # Example: total modules count.
-            ),
-            buttons=_main_help_menu,
-        )
-    except BotInlineDisabledError:
-        await ult.eor(get_string("help_3"))
-        
+@in_pattern("help", owner=False)
+async def inline_handler(event):
+    key = "Official"
+    count = 0
+    text = _strings.get(key, "").format(OWNER_NAME, HNDLR, len(HELP.get(key)))
+
+    result = await event.builder.article(
+        title="alive", text=text, buttons=page_num(count, key)
+    )
+    await event.answer([result], cache_time=0)
