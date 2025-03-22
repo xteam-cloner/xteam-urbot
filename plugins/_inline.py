e1fce2ce1b5af80e5af25c30d5d769e7bc62d44d
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-23 01:22:57 +0700

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -86,7 +86,7 @@ async def inline_alive(o):
     )
 
 
-"""@in_pattern("ultd", owner=True)
+@in_pattern("ultd", owner=True)
 async def inline_handler(event):
     z = []
     for x in LIST.values():
@@ -102,14 +102,14 @@ async def inline_handler(event):
             file=inline_pic(),
             link_preview=False,
             text=text,
-            buttons=_main_help_menu,
+            buttons=page_num,
         )
     else:
         result = await event.builder.article(
             title="Ultroid Help Menu", text=text, buttons=_main_help_menu
         )
     await event.answer([result], private=True, cache_time=300, gallery=True)
-"""
+
 
 @in_pattern("pasta", owner=True)
 async def _(event):
@@ -160,8 +160,7 @@ async def setting(event):
 _strings = {"Official": helps, "Addons": zhelps, "VCBot": get_string("inline_6")}
 
 
-#@callback(re.compile("uh_(.*)"), owner=True)
-@in_pattern("ultd(.*)"), owner=True)
+@callback(re.compile("uh_(.*)"), owner=True)
 async def help_func(ult):
     key, count = ult.data_match.group(1).decode("utf-8").split("_")
     if key == "VCBot" and HELP.get("VCBot") is None:
