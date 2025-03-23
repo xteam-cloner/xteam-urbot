7d0565ac80ec62675b9c1affd2646c39c30cdba4
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-23 15:43:09 +0700

    Update helper.py

--- plugins/helper.py
+++ plugins/helper.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 import base64
 import inspect
 from datetime import datetime
@@ -30,103 +37,88 @@ _main_help_menu = [
     [Button.inline("ᴄʟᴏꜱᴇ", data="close")],
 ]
 
-import re
-from telethon import events, Button, custom
-
-HELP = {}  # Assuming you have a dictionary like this
-
 @ultroid_cmd(pattern="helper( (.*)|$)")
-async def help_cmd(event):
-    module = event.pattern_match.group(1).strip()
-    if module in HELP:
-        await event.respond(HELP[module].__HELP__, parse_mode='html')
+async def _help(ult):
+    plug = ult.pattern_match.group(1).strip()
+    chat = await ult.get_chat()
+    if plug:
+        try:
+            if plug in HELP["Official"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["Official"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            elif HELP.get("Addons") and plug in HELP["Addons"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["Addons"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            elif HELP.get("VCBot") and plug in HELP["VCBot"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["VCBot"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            else:
+                try:
+                    x = get_string("help_11").format(plug)
+                    for d in LIST[plug]:
+                        x += HNDLR + d
+                        x += "\n"
+                    x += "\n© @xteam_cloner"
+                    await ult.eor(x)
+                except BaseException:
+                    file = None
+                    compare_strings = []
+                    for file_name in LIST:
+                        compare_strings.append(file_name)
+                        value = LIST[file_name]
+                        for j in value:
+                            j = cmd_regex_replace(j)
+                            compare_strings.append(j)
+                            if j.strip() == plug:
+                                file = file_name
+                                break
+                    if not file:
+                        # the entered command/plugin name is not found
+                        text = f"`{plug}` is not a valid plugin!"
+                        best_match = None
+                        for _ in compare_strings:
+                            if plug in _ and not _.startswith("_"):
+                                best_match = _
+                                break
+                        if best_match:
+                            text += f"\nDid you mean `{best_match}`?"
+                        return await ult.eor(text)
+                    output = f"**Command** `{plug}` **found in plugin** - `{file}`\n"
+                    if file in HELP["Official"]:
+                        for i in HELP["Official"][file]:
+                            output += i
+                    elif HELP.get("Addons") and file in HELP["Addons"]:
+                        for i in HELP["Addons"][file]:
+                            output += i
+                    elif HELP.get("VCBot") and file in HELP["VCBot"]:
+                        for i in HELP["VCBot"][file]:
+                            output += i
+                    output += "\n© @xteam_cloner"
+                    await ult.eor(output)
+        except BaseException as er:
+            LOGS.exception(er)
+            await ult.eor("Error 🤔 occurred.")
     else:
-        #await event.respond(f"<b>No Module Named <code>{module}</code></b>", parse_mode='html')
-    await event.respond(
-        "<b>Command & Help</b>",
-        buttons=paginate_modules(0, HELP, "help"),
-        parse_mode='html'
-    )
-
-def paginate_modules(page_number, module_dict, prefix):
-    modules = sorted(list(module_dict.keys()))
-    chunked_modules = [modules[i:i + 8] for i in range(0, len(modules), 8)]
-    if page_number == len(chunked_modules):
-        page_number = 0
-    elif page_number < 0:
-        page_number = len(chunked_modules) - 1
-    buttons = []
-    for module in chunked_modules[page_number]:
-        buttons.append(Button.inline(module.replace('_', ' '), data=f"help_module({module})"))
-    nav_buttons = []
-    if len(chunked_modules) > 1:
-        nav_buttons.append(Button.inline("<", data=f"help_prev({page_number - 1})"))
-        nav_buttons.append(Button.inline(">", data=f"help_next({page_number + 1})"))
-    if nav_buttons:
-        buttons.append(nav_buttons)
-    return buttons
-
-async def menu_callback(event):
-    data = event.data.decode()
-    mod_match = re.match(rb"help_module\((.+?)\)", event.data)
-    prev_match = re.match(rb"help_prev\((.+?)\)", event.data)
-    next_match = re.match(rb"help_next\((.+?)\)", event.data)
-    back_match = re.match(rb"help_back", event.data)
-    top_text = "<b>Command & Help</b>"
-
-    if mod_match:
-        module = mod_match.group(1).decode().replace(" ", "_")
-        text = HELP[module].__HELP__
-        await event.edit(text, buttons=Button.inline("Back", data=b"help_back"), parse_mode='html')
-    elif prev_match:
-        curr_page = int(prev_match.group(1).decode())
-        await event.edit(top_text, buttons=paginate_modules(curr_page, HELP, "help"), parse_mode='html')
-    elif next_match:
-        next_page = int(next_match.group(1).decode())
-        await event.edit(top_text, buttons=paginate_modules(next_page, HELP, "help"), parse_mode='html')
-    elif back_match:
-        await event.edit(top_text, buttons=paginate_modules(0, HELP, "help"), parse_mode='html')
-
-async def nothing_here(event):
-    buttons = [
-        [
-            Button.url("Deploy kangerBot", "https://t.me/Kangerbot?start"),
-            Button.url("Update Channel", "https://t.me/Kanger"),
-        ]
-    ]
-
-    await event.answer(
-        [
-            custom.InlineResult(
-                id="1",
-                type="article",
-                title="About KangerBot",
-                input_message=custom.InputTextMessageContent(
-                    """
-👾<b>KangerBot</b>
-
-Simple userbot telegram based in telethon.
-• Send messages to all groups/users simultaneously
-• Set a scheduled/reminder message
-• Manage and moderate the group
-• Respond to orders
-...And many more!
-""",
-                    parse_mode='html'
-                ),
-                description="Click this to see",
-                url="https://t.me/Kangerbot",
-                thumb= "https://telegra.ph//file/3838aa50510fd5020c2b3.jpg",
-                buttons=buttons,
+        try:
+            results = await ult.client.inline_query(asst.me.username, "_uh")
+        except BotMethodInvalidError:
+            return await ult.reply(
+                "Inline mode is disabled. Please enable it in bot settings or contact support.",
             )
-        ]
-    )
-
-# Register the handlers
-async def setup_handlers(client):
-    client.add_event_handler(help_cmd, events.NewMessage(pattern=r"(?i)^\.helper(?: |$)(.*)"))
-    client.add_event_handler(menu_callback, events.CallbackQuery(pattern=re.compile(rb"help_")))
-    client.add_event_handler(nothing_here, events.InlineQuery(pattern=r"user_help"))
-
-# Example of how to add a help command
-# HELP["example"] = type('HelpCommand', (), {"__HELP__": "This is an example help command."})
+        except BotResponseTimeoutError:
+            return await ult.eor(
+                "The bot did not respond in time. Please try again later.",
+            )
+        except BotInlineDisabledError:
+            return await ult.eor("The bot's inline mode is currently disabled.")
+        await results[0].click(chat.id, reply_to=ult.reply_to_msg_id, hide_via=True)
+        await ult.delete()
