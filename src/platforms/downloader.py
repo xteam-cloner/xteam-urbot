1f2bdb5a999028a8d9c50082282d6b6f114bc73a
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-27 12:24:04 +0700

    Update downloader.py

--- src/platforms/downloader.py
+++ src/platforms/downloader.py
@@ -4,7 +4,7 @@ from typing import Optional, Any
 
 from py_yt import VideosSearch, Playlist
 
-from pyUltroid import configs
+from pyUltroid.configs import Var
 from src.logger import LOGGER
 from src.platforms._httpx import HttpxClient
 from src.platforms._service import SpotifyDownload, YouTubeDownload
