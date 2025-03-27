440e0a1700991a764048ca481374c931fce2e013
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-27 12:20:16 +0700

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -57,3 +57,6 @@ class Var:
     DOWNLOAD_PFP_URL_CLOCK = config("DOWNLOAD_PFP_URL_CLOCK", default=False)
     DOWNLOADS_DIR = getenv("DOWNLOADS_DIR", "database/music")
     YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+    API_URL = getenv("API_URL", None)
+    API_KEY = getenv("API_KEY", None)
+    PROXY_URL = getenv("PROXY_URL", None)
