92c76846daa98b3969d4876a09e5a6cfa934e323
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-28 16:45:22 +0700

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,13 +1,13 @@
 from aiofiles import os
 
-import config
+from config import Var
 from src import client
 
 
 async def create_directories() -> None:
     """Create necessary directories."""
     try:
-        await os.makedirs(config.DOWNLOADS_DIR, exist_ok=True)
+        await os.makedirs(Var.DOWNLOADS_DIR, exist_ok=True)
         await os.makedirs("database/photos", exist_ok=True)
     except Exception as e:
         raise SystemExit(1) from e
