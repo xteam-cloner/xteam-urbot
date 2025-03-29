80b53c670c3c39c64ee7ea0a3b0c49317729bd88
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-03-29 08:03:50 +0700

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ SESSION_STRINGS = [
 ]
 
 "Your Telegram User ID"
-OWNER_ID = int(getenv("OWNER_ID", 5938660179))
+OWNER_ID = int(getenv("OWNER_ID", 1434595544))
 
 "Your MongoDB URI; get it from https://cloud.mongodb.com"
 MONGO_URI = getenv("MONGO_URI", None)
