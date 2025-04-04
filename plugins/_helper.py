70887ed3934e3cae2baba28db07b42bc56f04ef2
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-04 10:44:51 +0700

    Update _helper.py

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -37,104 +37,27 @@ _main_help_menu = [
     [Button.inline("ᴄʟᴏꜱᴇ", data="close")],
 ]
 
-@ultroid_cmd(pattern="helper( (.*)|$)")
-async def _help(ult):
-    plug = ult.pattern_match.group(1).strip()
-    chat = await ult.get_chat()
-    if plug:
-        try:
-            if plug in HELP["Official"]:
-                output = f"**Plugin** - `{plug}`\n"
-                for i in HELP["Official"][plug]:
-                    output += i
-                output += "\n© @xteam_cloner"
-                await ult.eor(output)
-            elif HELP.get("Addons") and plug in HELP["Addons"]:
-                output = f"**Plugin** - `{plug}`\n"
-                for i in HELP["Addons"][plug]:
-                    output += i
-                output += "\n© @xteam_cloner"
-                await ult.eor(output)
-            elif HELP.get("VCBot") and plug in HELP["VCBot"]:
-                output = f"**Plugin** - `{plug}`\n"
-                for i in HELP["VCBot"][plug]:
-                    output += i
-                output += "\n© @xteam_cloner"
-                await ult.eor(output)
-            else:
-                try:
-                    x = get_string("help_11").format(plug)
-                    for d in LIST[plug]:
-                        x += HNDLR + d
-                        x += "\n"
-                    x += "\n© @xteam_cloner"
-                    await ult.eor(x)
-                except BaseException:
-                    file = None
-                    compare_strings = []
-                    for file_name in LIST:
-                        compare_strings.append(file_name)
-                        value = LIST[file_name]
-                        for j in value:
-                            j = cmd_regex_replace(j)
-                            compare_strings.append(j)
-                            if j.strip() == plug:
-                                file = file_name
-                                break
-                    if not file:
-                        # the entered command/plugin name is not found
-                        text = f"`{plug}` is not a valid plugin!"
-                        best_match = None
-                        for _ in compare_strings:
-                            if plug in _ and not _.startswith("_"):
-                                best_match = _
-                                break
-                        if best_match:
-                            text += f"\nDid you mean `{best_match}`?"
-                        return await ult.eor(text)
-                    output = f"**Command** `{plug}` **found in plugin** - `{file}`\n"
-                    if file in HELP["Official"]:
-                        for i in HELP["Official"][file]:
-                            output += i
-                    elif HELP.get("Addons") and file in HELP["Addons"]:
-                        for i in HELP["Addons"][file]:
-                            output += i
-                    elif HELP.get("VCBot") and file in HELP["VCBot"]:
-                        for i in HELP["VCBot"][file]:
-                            output += i
-                    output += "\n© @xteam_cloner"
-                    await ult.eor(output)
-        except BaseException as er:
-            LOGS.exception(er)
-            await ult.eor("Error 🤔 occurred.")
+@ultroid_cmd(pattern="helper")
+async def inline_handler(event):
+    z = []
+    for x in LIST.values():
+        z.extend(x)
+    text = get_string("inline_4").format(
+        OWNER_NAME,
+        len(HELP.get("Official", [])),
+        len(HELP.get("Addons", [])),
+        len(z),
+    )
+    if inline_pic():
+        result = await event.builder.photo(
+            file=inline_pic(),
+            link_preview=False,
+            text=text,
+            buttons=page_num,
+        )
     else:
-        try:
-            results = await ult.client.inline_query(asst.me.username, "_uh")
-        except BotMethodInvalidError:
-            return await ult.reply(
-                "Inline mode is disabled. Please enable it in bot settings or contact support.",
-            )
-        except BotResponseTimeoutError:
-            return await ult.eor(
-                "The bot did not respond in time. Please try again later.",
-            )
-        except BotInlineDisabledError:
-            return await ult.eor("The bot's inline mode is currently disabled.")
-        await results[0].click(chat.id, reply_to=ult.reply_to_msg_id, hide_via=True)
-        await ult.delete()
-
-import re
-
-@in_pattern(re.compile("uh_(.*)"))
-async def help_func(ult):
-    key, count = ult.data_match.group(1).decode("utf-8").split("_")
-    if key == "VCBot" and HELP.get("VCBot") is None:
-        return await ult.answer(get_string("help_12"), alert=True)
-    elif key == "Addons" and HELP.get("Addons") is None:
-        return await ult.answer(get_string("help_13").format(HNDLR), alert=True)
-    if "|" in count:
-        _, count = count.split("|")
-    count = int(count) if count else 0
-    text = _strings.get(key, "").format(OWNER_NAME, len(HELP.get(key)))
-    await ult.edit(text, buttons=page_num(count, key), link_preview=False)
+        result = await event.builder.article(
+            title="Ultroid Help Menu", text=text, buttons=_main_help_menu
+        )
+    await event.answer([result], private=True, cache_time=300, gallery=True)
     
