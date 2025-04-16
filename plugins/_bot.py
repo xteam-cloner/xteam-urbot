6f88acd3d5ad890a2e88c2aa8d72e6d57702f353
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-16 18:41:51 +0700

    Update and rename bot.py to _bot.py

--- plugins/_bot.py
+++ plugins/_bot.py
@@ -32,7 +32,7 @@ except ImportError:
     Repo = None
 
 from telethon.utils import resolve_bot_file_id
-from config import Var
+from xteam.configs import Var
 from . import (
     ATRA_COL,
     LOGS,
@@ -91,102 +91,6 @@ async def alive(event):
     text = alive_txt.format(ultroid_version, UltVer, __version__)
     await event.answer(text, alert=True)
 
-
-"""@ultroid_cmd(
-    pattern="alive( (.*)|$)",
-)
-async def lol(ult):
-    match = ult.pattern_match.group(1).strip()
-    inline = None
-    if match in ["inline", "i"]:
-        try:
-            res = await ult.client.inline_query(asst.me.username, "alive")
-            return await res[0].click(ult.chat_id)
-        except BotMethodInvalidError:
-            pass
-        except BaseException as er:
-            LOGS.exception(er)
-        inline = True
-    pic = udB.get_key("ALIVE_PIC")
-    if isinstance(pic, list):
-        pic = choice(pic)
-    uptime = time_formatter((time.time() - start_time) * 1000)
-    header = udB.get_key("ALIVE_TEXT") or get_string("bot_1")
-    y = Repo().active_branch
-    xx = Repo().remotes[0].config_reader.get("url")
-    rep = xx.replace(".git", f"/tree/{y}")
-    kk = f" `[{y}]({rep})` "
-    if inline:
-        kk = f"<a href={rep}>{y}</a>"
-        parse = "html"
-        als = in_alive.format(
-            header,
-            f"{ultroid_version} [{HOSTED_ON}]",
-            UltVer,
-            pyver(),
-            uptime,
-            kk,
-        )
-
-        if _e := udB.get_key("ALIVE_EMOJI"):
-            als = als.replace("🌀", _e)
-    else:
-        parse = "md"
-        als = (get_string("alive_1")).format(
-            header,
-            OWNER_NAME,
-            f"{ultroid_version} [{HOSTED_ON}]",
-            UltVer,
-            uptime,
-            pyver(),
-            __version__,
-            kk,
-        )
-
-        if a := udB.get_key("ALIVE_EMOJI"):
-            als = als.replace("✵", a)
-    if pic:
-        try:
-            await ult.reply(
-                als,
-                file=pic,
-                parse_mode=parse,
-                link_preview=False,
-                buttons=buttons if inline else None,
-            )
-            return await ult.try_delete()
-        except ChatSendMediaForbiddenError:
-            pass
-        except BaseException as er:
-            LOGS.exception(er)
-            try:
-                await ult.reply(file=pic)
-                await ult.reply(
-                    als,
-                    parse_mode=parse,
-                    buttons=buttons if inline else None,
-                    link_preview=False,
-                )
-                return await ult.try_delete()
-            except BaseException as er:
-                LOGS.exception(er)
-    await eor(
-        ult,
-        als,
-        parse_mode=parse,
-        link_preview=False,
-        buttons=buttons if inline else None,
-    )
-
-
-@ultroid_cmd(pattern="ping$", chats=[], type=["official", "assistant"])
-async def _(event):
-    start = time.time()
-    x = await event.eor("Pong !")
-    end = round((time.time() - start) * 1000)
-    uptime = time_formatter((time.time() - start_time) * 1000)
-    await x.edit(get_string("ping").format(end, uptime))
-"""
 
 @ultroid_cmd(
     pattern="cmds$",
