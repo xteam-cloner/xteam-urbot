d287df0dce68145d9961455a90e42f2c6f200634
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-16 18:30:36 +0700

    Update asstcmd.py

--- plugins/asstcmd.py
+++ plugins/asstcmd.py
@@ -11,8 +11,8 @@ __doc__ = get_help("help_asstcmd")
 
 import os
 
-from pyUltroid.dB.asstcmd_db import add_cmd, cmd_reply, list_cmds, rem_cmd
-from pyUltroid.fns.tools import create_tl_btn, format_btn, get_msg_button
+from xteam.dB.asstcmd_db import add_cmd, cmd_reply, list_cmds, rem_cmd
+from xteam.fns.tools import create_tl_btn, format_btn, get_msg_button
 
 try:
     from telegraph import upload_file as uf
