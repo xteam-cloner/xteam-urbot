f5d8c888744a2b4b685a8239f8a78c7a724f6927
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-17 23:35:23 +0700

    Update globaltools.py

--- plugins/globaltools.py
+++ plugins/globaltools.py
@@ -44,9 +44,9 @@ from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.functions.contacts import BlockRequest, UnblockRequest
 from telethon.tl.types import ChatAdminRights, User
 
-from pyUltroid.dB import DEVLIST
-from pyUltroid.dB.base import KeyManager
-from pyUltroid.dB.gban_mute_db import (
+from xteam.dB import DEVLIST
+from xteam.dB.base import KeyManager
+from xteam.dB.gban_mute_db import (
     gban,
     gmute,
     is_gbanned,
@@ -55,7 +55,7 @@ from pyUltroid.dB.gban_mute_db import (
     ungban,
     ungmute,
 )
-from pyUltroid.fns.tools import create_tl_btn, format_btn, get_msg_button
+from xteam.fns.tools import create_tl_btn, format_btn, get_msg_button
 
 from . import (
     HNDLR,
