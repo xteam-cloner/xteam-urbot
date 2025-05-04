91cc165f24673efbaf8865eb327e142c7ba8f5f8
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-04 10:51:42 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -123,3 +123,123 @@ async def _help(ult):
             return await ult.eor("The bot's inline mode is currently disabled.")
         await results[0].click(chat.id, reply_to=ult.reply_to_msg_id, hide_via=True)
         await ult.delete()
+
+Import base64
+import inspect
+from datetime import datetime
+from html import unescape
+from random import choice
+from re import compile as re_compile
+
+from bs4 import BeautifulSoup as bs
+from telethon import Button
+from telethon.tl.alltlobjects import LAYER, tlobjects
+from telethon.tl.types import DocumentAttributeAudio as Audio
+from telethon.tl.types import InputWebDocument as wb
+
+from telethon.errors.rpcerrorlist import (
+    BotInlineDisabledError,
+    BotMethodInvalidError,
+    BotResponseTimeoutError,
+)
+from telethon.tl.custom import Button
+from assistant import *
+from xteam.dB._core import HELP, LIST
+from xteam.fns.tools import cmd_regex_replace
+
+from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, inline_pic, udB, ultroid_cmd, call_back, callback, def_logs, eor, get_string, heroku_logs, in_pattern
+
+_main_help_menu = [
+    [
+        Button.url("18+",url=f"https://t.me/+5WS60Gdkpt84YWM1"),
+        Button.inline("ᴍᴏᴅᴜʟᴇꜱ", data="uh_Official_"),
+    ],
+    [Button.inline("ᴄʟᴏꜱᴇ", data="close")],
+]
+
+@ultroid_cmd(pattern="helper( (.*)|$)")
+async def _help(ult):
+    plug = ult.pattern_match.group(1).strip()
+    chat = await ult.get_chat()
+    if plug:
+        try:
+            if plug in HELP["Official"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["Official"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            elif HELP.get("Addons") and plug in HELP["Addons"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["Addons"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            elif HELP.get("VCBot") and plug in HELP["VCBot"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["VCBot"][plug]:
+                    output += i
+                output += "\n© @xteam_cloner"
+                await ult.eor(output)
+            else:
+                try:
+                    x = get_string("help_11").format(plug)
+                    for d in LIST[plug]:
+                        x += HNDLR + d
+                        x += "\n"
+                    x += "\n© @xteam_cloner"
+                    await ult.eor(x)
+                except BaseException:
+                    file = None
+                    compare_strings = []
+                    for file_name in LIST:
+                        compare_strings.append(file_name)
+                        value = LIST[file_name]
+                        for j in value:
+                            j = cmd_regex_replace(j)
+                            compare_strings.append(j)
+                            if j.strip() == plug:
+                                file = file_name
+                                break
+                    if not file:
+                        # the entered command/plugin name is not found
+                        text = f"`{plug}` is not a valid plugin!"
+                        best_match = None
+                        for _ in compare_strings:
+                            if plug in _ and not _.startswith("_"):
+                                best_match = _
+                                break
+                        if best_match:
+                            text += f"\nDid you mean `{best_match}`?"
+                        return await ult.eor(text)
+                    output = f"**Command** `{plug}` **found in plugin** - `{file}`\n"
+                    if file in HELP["Official"]:
+                        for i in HELP["Official"][file]:
+                            output += i
+                    elif HELP.get("Addons") and file in HELP["Addons"]:
+                        for i in HELP["Addons"][file]:
+                            output += i
+                    elif HELP.get("VCBot") and file in HELP["VCBot"]:
+                        for i in HELP["VCBot"][file]:
+                            output += i
+                    output += "\n© @xteam_cloner"
+                    await ult.eor(output)
+        except BaseException as er:
+            LOGS.exception(er)
+            await ult.eor("Error 🤔 occurred.")
+    else:
+        try:
+            results = await ult.client.inline_query(asst.me.username, "uh_")
+        except BotMethodInvalidError:
+            return await ult.reply(
+                "Inline mode is disabled. Please enable it in bot settings or contact support.",
+            )
+        except BotResponseTimeoutError:
+            return await ult.eor(
+                "The bot did not respond in time. Please try again later.",
+            )
+        except BotInlineDisabledError:
+            return await ult.eor("The bot's inline mode is currently disabled.")
+        # Langsung klik tombol "ᴍᴏᴅᴜʟᴇꜱ" dari hasil inline query pertama
+        await results[0].click(chat.id, reply_to=ult.reply_to_msg_id, hide_via=True)
+        await ult.delete()
