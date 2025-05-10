1b16b3febfaea8f3733a4e1923c84bac4b0f5dc2
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-10 21:47:16 +0700

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -74,7 +74,7 @@ async def _help(event):
                     if best_match:
                         text += f"\nDid you mean {best_match}?"
                     return await event.eor(text)
-                output = f"Command {plug} found in plugin - {file}\n"
+                output = f"Command `{plug}` found in plugin - `{file}`\n"
                 if HELP.get("Official") and file in HELP["Official"]:
                     for i in HELP["Official"][file]:
                         output += i
