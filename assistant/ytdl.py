d9227a4a468153a62a82e152642a810a382eb2c7
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-17 01:19:19 +0700

    Update ytdl.py

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -18,14 +18,14 @@ from telethon.errors.rpcerrorlist import FilePartLengthInvalidError, MediaEmptyE
 from telethon.tl.types import DocumentAttributeAudio, DocumentAttributeVideo
 from telethon.tl.types import InputWebDocument as wb
 
-from pyUltroid.fns.helper import (
+from xteam.fns.helper import (
     bash,
     fast_download,
     humanbytes,
     numerize,
     time_formatter,
 )
-from pyUltroid.fns.ytdl import dler, get_buttons, get_formats
+from xteam.fns.ytdl import dler, get_buttons, get_formats
 
 from . import LOGS, asst, callback, in_pattern, udB
 
