d0fc750a157b8257646ed1e15eedbda6088ba415
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-27 20:21:25 +0700

    Update autopic.py

--- plugins/autopic.py
+++ plugins/autopic.py
@@ -31,7 +31,7 @@ async def autopic(e):
     if not search:
         return await e.eor(get_string("autopic_1"), time=5)
     e = await e.eor(get_string("com_1"))
-    gi = googleimagesdownload()
+    gi = google_images_download()
     args = {
         "keywords": search,
         "limit": 50,
