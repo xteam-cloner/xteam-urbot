cd59336fcddca974d4bfbe4bbd4c6e51cb2a3249
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-09-11 15:35:34 +0700

    Update writer.py

--- plugins/writer.py
+++ plugins/writer.py
@@ -82,13 +82,13 @@ async def writer(e):
     k = await e.eor(get_string("com_1"))
     img = Image.open("resources/extras/—Pngtree—a plain notepad with flowers_13244509.jpg.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("resources/fonts/assfont.ttf", 25)
+    font = ImageFont.truetype("resources/fonts/assfont.ttf", 30)
     x, y = 150, 140
     lines = text_set(text)
     line_height = font.getbbox("hg")[3] - font.getbbox("hg")[1]
     for line in lines:
         draw.text((x, y), line, fill=(1, 22, 55), font=font)
-        y = y + line_height - 10
+        y = y + line_height - 5
     file = "ult.jpg"
     img.save(file)
     await e.reply(file=file)
