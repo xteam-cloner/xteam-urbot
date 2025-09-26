e0851d9af1bed63217551bc03f56d219dd935f32
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-09-26 19:33:21 +0700

    Update pafk.py

--- plugins/pafk.py
+++ plugins/pafk.py
@@ -1,12 +1,20 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 from . import get_help
 
-doc = get_help("help_afk")
+__doc__ = get_help("help_afk")
 
 
 import asyncio
 
-from telegraph import upload_file as uf
 from telethon import events
+from telethon import Button
+from telegraph import upload_file as uf
 
 from xteam.dB.afk_db import add_afk, del_afk, is_afk
 from xteam.dB.base import KeyManager
@@ -19,15 +27,17 @@ from . import (
     get_string,
     mediainfo,
     udB,
-    ultroid_bot,
     ultroid_cmd,
 )
 
-from telethon import Button, events
+old_afk_msg = []
 
-@ultroid_cmd(pattern="pafk( (.*)|$)", owner_only=True)
+is_approved = KeyManager("PMPERMIT", cast=list).contains
+
+
+@ultroid_cmd(pattern="afkb( (.*)|$)", owner_only=True)
 async def set_afk(event):
-    if event.client._bot or is_afk():
+    if asst._bot or is_afk():
         return
     text, media, media_type = None, None, None
     if event.pattern_match.group(1).strip():
@@ -44,7 +54,7 @@ async def set_afk(event):
                 media = f"https://graph.org{iurl[0]}"
             else:
                 media = reply.file.id
-    await event.eor("Done", time=2)
+    await event.eor("`Done`", time=2)
     add_afk(text, media_type, media)
     asst.add_handler(remove_afk, events.NewMessage(outgoing=True))
     asst.add_handler(
@@ -53,46 +63,108 @@ async def set_afk(event):
             incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
         ),
     )
-    
     msg1, msg2 = None, None
-    
-    # Membuat tombol "Selesai" dengan callback data
-    selesai_button = Button.inline('✅ Selesai', data=b'disable_afk')
-
     if text and media:
         if "sticker" in media_type:
-            msg1 = await asst.send_file(event.chat_id, file=media, buttons=[[selesai_button]])
+            msg1 = await asst.send_file(event.chat_id, file=media)
             msg2 = await asst.send_message(
                 event.chat_id, get_string("afk_5").format(text)
             )
         else:
             msg1 = await asst.send_message(
-                event.chat_id, get_string("afk_5").format(text), file=media, buttons=[[selesai_button]]
+                event.chat_id, get_string("afk_5").format(text), file=media
             )
     elif media:
         if "sticker" in media_type:
-            msg1 = await asst.send_file(event.chat_id, file=media, buttons=[[selesai_button]])
+            msg1 = await asst.send_file(event.chat_id, file=media)
             msg2 = await asst.send_message(event.chat_id, get_string("afk_6"))
         else:
             msg1 = await asst.send_message(
-                event.chat_id, get_string("afk_6"), file=media, buttons=[[selesai_button]]
+                event.chat_id, get_string("afk_6"), file=media
             )
     elif text:
-        msg1 = await event.respond(get_string("afk_5").format(text), buttons=[[selesai_button]])
+        msg1 = await event.respond(get_string("afk_5").format(text))
     else:
-        msg1 = await event.respond(get_string("afk_6"), buttons=[[selesai_button]])
-        
+        msg1 = await event.respond(get_string("afk_6"))
     old_afk_msg.append(msg1)
     if msg2:
         old_afk_msg.append(msg2)
         return await asst.send_message(LOG_CHANNEL, msg2.text)
     await asst.send_message(LOG_CHANNEL, msg1.text)
 
-# Handler untuk tombol "Selesai"
-@asst.on(events.CallbackQuery(data=b'disable_afk'))
-async def disable_afk_handler(event):
+
+async def remove_afk(event):
+    if event.is_private and udB.get_key("PMSETTING") and not is_approved(event.chat_id):
+        return
+    elif "afk" in event.text.lower():
+        return
+    elif event.chat_id in NOSPAM_CHAT:
+        return
     if is_afk():
-        await remove_afk(event)
-        await event.edit("Mode AFK telah dinonaktifkan.")
+        _, _, _, afk_time = is_afk()
+        del_afk()
+        message_link = f"https://t.me/c/{event.chat_id}/{event.id}"
+        button = Button.url("I'm back", message_link)
+        off = await event.reply(
+            get_string("afk_1").format(afk_time), buttons=button
+        )
+        await asst.send_message(LOG_CHANNEL, get_string("afk_2").format(afk_time))
+        for x in old_afk_msg:
+            try:
+                await x.delete()
+            except BaseException:
+                pass
+        await asyncio.sleep(10)
+        await off.delete()
+
+
+async def on_afk(event):
+    if event.is_private and Redis("PMSETTING") and not is_approved(event.chat_id):
+        return
+    elif "afk" in event.text.lower():
+        return
+    elif not is_afk():
+        return
+    if event.chat_id in NOSPAM_CHAT:
+        return
+    sender = await event.get_sender()
+    if sender.bot or sender.verified:
+        return
+    text, media_type, media, afk_time = is_afk()
+    msg1, msg2 = None, None
+    if text and media:
+        if "sticker" in media_type:
+            msg1 = await event.reply(file=media)
+            msg2 = await event.reply(get_string("afk_3").format(afk_time, text))
+        else:
+            msg1 = await event.reply(
+                get_string("afk_3").format(afk_time, text), file=media
+            )
+    elif media:
+        if "sticker" in media_type:
+            msg1 = await event.reply(file=media)
+            msg2 = await event.reply(get_string("afk_4").format(afk_time))
+        else:
+            msg1 = await event.reply(get_string("afk_4").format(afk_time), file=media)
+    elif text:
+        msg1 = await event.reply(get_string("afk_3").format(afk_time, text))
     else:
-        await event.edit("Anda tidak dalam mode AFK.")
+        msg1 = await event.reply(get_string("afk_4").format(afk_time))
+    for x in old_afk_msg:
+        try:
+            await x.delete()
+        except BaseException:
+            pass
+    old_afk_msg.append(msg1)
+    if msg2:
+        old_afk_msg.append(msg2)
+
+
+if udB.get_key("AFK_DB"):
+    asst.add_handler(remove_afk, events.NewMessage(outgoing=True))
+    asst.add_handler(
+        on_afk,
+        events.NewMessage(
+            incoming=True, func=lambda e: bool(e.mentioned or e.is_private)
+        ),
+    )
