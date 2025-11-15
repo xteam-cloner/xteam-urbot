b50a1f8e7bba0cf83a79ff66c6803d102e7748e1
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-11-15 12:55:58 +0700

    Update vcplug.py

--- plugins/vcplug.py
+++ plugins/vcplug.py
@@ -12,7 +12,8 @@ from dataclasses import dataclass
 from typing import Dict, Optional, Tuple, Union, Any
 
 import httpx
-from . import * from telethon import events, TelegramClient 
+from . import * 
+from telethon import events, TelegramClient 
 from telethon.tl.types import Message
 # Menggunakan Var dari xteam.configs
 from xteam.configs import Var 
