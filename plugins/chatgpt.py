6d644f8e93404571c837ecf779bf11aadae12754
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-11-19 15:48:16 +0700

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -37,14 +37,14 @@ from . import *
 ENDPOINTS = {
     "gpt": "https://api.openai.com/v1/chat/completions",
     "antr": "https://api.anthropic.com/v1/messages",
-    "gemini": "https://generativelanguage.googleapis.com/v1/models/gemini-2.5",
+    "gemini": "https://generativelanguage.googleapis.com/v1beta/chat/completions",
     "deepseek": "https://api.deepseek.com/v3/chat/completions"
 }
 
 DEFAULT_MODELS = {
     "gpt": "gpt-3.5-turbo",
     "antr": "claude-3-haiku",
-    "gemini": "gemini-2.5",
+    "gemini": "gemini-1.5-flash",
     "deepseek": "DeepSeek-V3"
 }
 
