0e40969fa20184a4010e522e23381658e778fa60
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-01 09:58:08 +0700

    Update group.py

--- plugins/group.py
+++ plugins/group.py
@@ -2,12 +2,8 @@ from telethon.tl.types import Channel, Chat, User
 from . import ultroid_cmd, ultroid_bot 
 
 client = ultroid_bot 
-cmd = ultroid_cmd.command 
 
-@client.on(cmd("totalgrup", channel=False, cmd_help={
-    "help": "Menghitung total grup dan channel yang diikuti akun.",
-    "example": "{cmd}"
-}))
+@ultroid_cmd(pattern="totalgroup( (.*)|$)", fullsudo=True)
 async def hitung_total_grup_plugin(event):
     await event.edit("🔄 Mulai menghitung semua dialog...")
 
@@ -29,18 +25,17 @@ async def hitung_total_grup_plugin(event):
         elif isinstance(entity, Chat): 
             total_grup_kecil += 1
             
-    total_grup_channel = total_grup_kecil + total_supergroup + total_channel_broadcast
+    total_semua_grup = total_grup_kecil + total_supergroup
+    total_grup_channel = total_semua_grup + total_channel_broadcast
     
     output = (
-        "**📊 Hasil Total Entitas yang Diikuti**\n"
+        "<blockquote>📊 Hasil Total Entitas\n"
         "--------------------------------------------------\n"
-        f"👤 Total Obrolan Pribadi (Users): `{total_chat_pribadi}`\n"
-        #f"✅ Total Grup Kecil (Legacy Chat): `{total_grup_kecil}`\n"
-        f"🚀 Total Group : `{total_grup_kecil + total_supergroup}`\n"
-        f"📣 Total Channel : `{total_channel_broadcast}`\n"
+        f"👤 Total Obrolan Pribadi : {total_chat_pribadi}\n"
+        f"👥 Total Group : {total_semua_grup}\n"
+        f"📣 Total Channel : {total_channel_broadcast}\n"
         "--------------------------------------------------\n"
-        f"✨ **TOTAL SELURUH GRUP/CHANNEL:** `{total_grup_channel}` entitas"
+        f"✨ TOTAL SELURUH GRUP & CHANNEL: {total_grup_channel} entitas"</blockquote>
     )
 
-    await event.edit(output)
-  
+    await event.edit(output, parse_mode="html")
