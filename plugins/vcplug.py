e83eb914b7aee7a1e962ec87b25843432f72019b
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-12-11 19:39:22 +0700

    Update vcplug.py

--- plugins/vcplug.py
+++ plugins/vcplug.py
@@ -66,6 +66,25 @@ from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from xteam.configs import Var # Asumsi ASSISTANT_ID ada di Var
 
+
+import functools
+
+def is_admin(func):
+    @functools.wraps(func)
+    async def a_c(event):
+        is_admin = False
+        if not event.is_private:
+            try:
+                _s = await event.client.get_permissions(event.chat_id, event.sender_id)
+                if _s.is_admin:
+                    is_admin = True
+            except:
+                is_admin = False
+        if is_admin:
+            await func(event, _s)
+        else:
+            await event.reply("Only Admins can execute this command!")
+    return a_c
 # Dapatkan ID asisten dari konfigurasi
 ASSISTANT_ID = Var.ASSISTANT_ID # Ganti Var dengan sumber config yang benar!
 
